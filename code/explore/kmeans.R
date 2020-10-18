89b503ef0f6e9d0dcb38c28297a86752e2362b3d
Alex Abraham <alex.abraham13@gmail.com>
2020-10-18 02:05:10 -0500

    kmeans with alt indicators (std)

--- code/explore/kmeans.R
+++ code/explore/kmeans.R
@@ -1,20 +1,41 @@
 #kmeans.R
 
-
-#pre-screening for final clusters data ---------
-
-cities_keep <- inmoves_scholars_age_sub_crs %>% 
-  dplyr::filter(pop_total.1 > 1e6)
-
-df_cluster <- cities_keep %>%  
-  dplyr::select(-c(MET2013_chr, pop_total.1, `N/A`))
-
-#end pre-screen ---------
+#uses by-degree inmove rates as inputs for clustering.
+#F, then use alternative indicators as inputs
+use_inmove_rates <- F
 
 
-#how might majors cluster together?
-km <- kmeans(t(df_cluster), centers = 10)
-# View(data.frame("cluster" = km$cluster, "degree" = colnames(df_cluster)))
+
+if (use_inmove_rates) {
+  
+  #pre-screening for final clusters data ---------
+  
+  cities_keep <- inmoves_scholars_age_sub_crs %>% 
+    dplyr::filter(pop_total.1 > 1e6)
+  
+  df_cluster <- cities_keep %>%  
+    dplyr::select(-c(MET2013_chr, pop_total.1, `N/A`))
+  
+  #end pre-screen ---------
+  
+  #how might majors cluster together?
+  km <- kmeans(t(df_cluster), centers = 10)
+  # View(data.frame("cluster" = km$cluster, "degree" = colnames(df_cluster)))
+
+} else {
+  
+  df_full <- read_csv("data_raw/DataFinal_vc.csv")[, -1]
+  cities_keep <- df_full[complete.cases(df_full), ]
+  df_cluster <- cities_keep %>% 
+    dplyr::select(-c(City, lat, lng))
+  
+  df_cluster <- 
+    apply(df_cluster, MARGIN = 2, function(x) ( x - mean(x) ) / sd(x) )
+
+}
+
+
+
 
 
 
@@ -33,9 +54,12 @@ calc_ch_index <- function(df_cluster, K) {
 }
 ch_index <- 
   unlist( lapply(2:20, df_cluster = df_cluster, FUN = calc_ch_index) )
-which.max(ch_index)
 
-km <- kmeans(df_cluster, centers = 3)
+km <- kmeans(df_cluster, centers = which.max(ch_index) + 1)
+
+
+
+
 
 
 
@@ -45,19 +69,22 @@ km <- kmeans(df_cluster, centers = 3)
 df_explore1 <- cbind("cluster" = km$cluster, cities_keep)
 write_csv(df_explore1, "data_processed/select_msa_clusters_grad_inmove_rates.csv")
 
-df_explore <- df_explore1 %>% 
-  gather(key = DEGFIELD, value = inmove_per_thous, 
-         -c(cluster, MET2013_chr, pop_total.1))
+if (use_inmove_rates) {
 
-p <- 
-  ggplot(df_explore %>% 
-           dplyr::filter(DEGFIELD != "N/A")) + 
+  df_explore <- df_explore1 %>% 
+    gather(key = DEGFIELD, value = inmove_per_thous, 
+           -c(cluster, MET2013_chr, pop_total.1))
+  
+  p <- 
+    ggplot(df_explore %>% 
+             dplyr::filter(DEGFIELD != "N/A")) + 
     geom_boxplot(aes(DEGFIELD, inmove_per_thous, 
                      group = interaction(DEGFIELD, cluster), 
                      color = factor(cluster))) + 
     coord_flip() + 
     theme(legend.position = "top")
-pdf("plots/inmove_rates_by_cluster_major.pdf", width = 12, height = 12)
-print(p)
-dev.off()
+  pdf("plots/inmove_rates_by_cluster_major.pdf", width = 12, height = 12)
+  print(p)
+  dev.off()
   
+} else df_explore <- df_explore1
